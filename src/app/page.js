0befcf70c45d150962fb8c677dbd79ade498834e
Piyush Gupta <piyushgupta941d@gmail.com>
2023-12-11 13:39:30 +0530

    added all points

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,58 @@
 'use client'
 
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
+import { Suspense, useEffect, useState, useRef } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { useGLTF, OrbitControls } from '@react-three/drei'
+import * as THREE from 'three'
+import gsap from 'gsap'
 
 export default function Home() {
+
+  const [cameraPathPoints, setCameraPathPoints] = useState([])
+
+  useEffect(() => {
+    fetch('/points.json')
+      .then(res => res.json())
+      .then(data => setCameraPathPoints(data))
+  },[])
+
+  const Render = () => {
+    const camera = useThree((state) => state.camera)
+
+    let index = 0
+    const {x, y, z} = cameraPathPoints.position[index] 
+    camera.position.set(x, y, z)
+
+    const {x: rx, y: ry, z: rz} = cameraPathPoints.rotation[index]
+    camera.rotation.set(rx, ry, rz)
+
+    let timeout
+    const handleScroll = (e) => {
+      if(timeout) clearTimeout(timeout)
+      timeout = setTimeout(() => {
+        if (e.deltaY > 0) {
+          index = index + 1
+          if (index > cameraPathPoints.position.length - 1) {
+            index = cameraPathPoints.position.length - 1
+          }
+        } else {
+          index = index - 1
+          if (index < 0) {
+            index = 0
+          }
+        }
+
+          const {x, y, z} = cameraPathPoints.position[index] 
+          gsap.to(camera.position, {duration: 2, x, y, z})
   
+          const {x: rx, y: ry, z: rz} = cameraPathPoints.rotation[index]
+          gsap.to(camera.rotation, {x: rx, y: ry, z: rz, duration: 2})
+      }, 200)
+    }
+
+    window.addEventListener('wheel', handleScroll)
+  }
+
   const Model = () => {
     const gltf = useGLTF('possum_springs.glb')
     gltf.scene.rotation.y = -1.55
@@ -15,10 +62,11 @@ export default function Home() {
   return (
     <div className="w-screen h-screen">
       <Suspense fallback={'loading...'}>
-        <Canvas camera={{position: [0,20,200]}}>
+        <Canvas>
           <ambientLight intensity={1} />
           <Model />
-          <OrbitControls />
+          <Render />
+          {/* <OrbitControls /> */}
         </Canvas>
       </Suspense>
     </div>
