ccd8880cbd4843d072b36508cbc397366f53a01b
Piyush Gupta <piyushgupta941d@gmail.com>
2023-12-16 23:11:10 +0530

    project section implemented

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -2,10 +2,17 @@ import { Canvas } from '@react-three/fiber'
 import { Text, Html } from '@react-three/drei'
 import { useState } from 'react';
 import Image from 'next/image';
+import ProjectItem from './ProjectItem';
+
+import journalApp from 'public/assets/projects/journal-app.png'
+import houseMarketPlace from 'public/assets/projects/house-marketplace.png'
+import moviesTrailer from 'public/assets/projects/movies-trailer.png'
+import githubFinder from 'public/assets/projects/github-finder.png'
 
 export default (x, y, z) => {
 
   const [opened, setOpened] = useState(false)
+  const [projectNumber, setProjectNumber] = useState(0)
 
   const onClick = () => {
     setOpened(true)
@@ -13,11 +20,43 @@ export default (x, y, z) => {
 
   const onClose = () => {
     setOpened(false)
+    setProjectNumber(0)
   }
 
   const onNext = () => {
-    console.log('next')
+    setProjectNumber(projectNumber + 1)
   }
+
+  const onPrev = () => {
+    setProjectNumber(projectNumber - 1)
+  }
+
+  const info = [
+    {
+      title: 'Polaroid Gallery',
+      img: journalApp,
+      projectUrl: 'https://www.piyushg.com/journalApp',
+      tech: 'React Native'
+    },
+    {
+      title: 'House MarketPlace',
+      img: houseMarketPlace,
+      projectUrl: 'https://www.piyushg.com/houseMarketplace',
+      tech: 'React JS'
+    },
+    {
+      title: 'Movies-Trailer',
+      img: moviesTrailer,
+      projectUrl: 'https://www.piyushg.com/moviesTrailer',
+      tech: 'React Native'
+    },
+    {
+      title: 'Github Finder',
+      img: githubFinder,
+      projectUrl: 'https://www.piyushg.com/githubFinder',
+      tech: 'React JS'
+    },
+  ]
 
   return (
     !opened ?
@@ -30,16 +69,29 @@ export default (x, y, z) => {
         </div>
       </Html>
       :
-      <Html occlude distanceFactor={10} position={[x , y + 0.8, z - 10]} rotation={[-0.1, 0, 0]} transform>
+      <Html occlude distanceFactor={10} position={[x , y, z - 10]} rotation={[-0.1, 0, 0]} transform>
         <div className="container">
-          <div className='bg-[#9e2424] w-80 rounded-md border-4 p-2 border-[#732402]'>
-            <p className="text-white text-base">Hi, I'm Piyush a Full-Stack Developer</p>
-            <p className="text-white text-base">specializing in building and designing exceptional digital experiences. Currently, I'm focused on building responsive 3D full stack web applications.</p>
+          <div className='bg-[#9e2424] w-[30rem] rounded-md border-4 p-2 border-[#732402] inline-block'>
+            <ProjectItem title={info[projectNumber].title} img={info[projectNumber].img} projectUrl={info[projectNumber].projectUrl} tech={info[projectNumber].tech} />
           </div>
-          <div className='absolute -right-12 top-[4.7rem] bg-[#fff] h-6 w-6'></div>
-          <div className='absolute -right-16 top-16'>
-            <button onClick={onNext}><Image src='/images/next.png' width={50} height={50} alt='Next icon' /></button>
-          </div>
+          {
+            projectNumber < info.length - 1 &&
+            <>
+            <div className='absolute -right-12 top-[7.7rem] bg-[#fff] h-6 w-6'></div>
+            <div className='absolute -right-16 top-[7rem]'>
+              <button onClick={onNext}><Image src='/images/next.png' width={50} height={50} alt='Next icon' /></button>
+            </div>
+            </>
+          }
+          {
+            projectNumber > 0 &&
+            <>
+              <div className='absolute -left-12 top-[7.7rem] bg-[#fff] h-6 w-6'></div>
+              <div className='absolute -left-16 top-[7rem]'>
+                <button onClick={onPrev}><Image src='/images/next.png' width={50} height={50} alt='Next icon' style={{ transform: 'rotate(180deg)' }} /></button>
+              </div>
+            </>
+          }
           <div className='absolute -top-3 -right-2 bg-[#fff] h-4 w-4'></div>
           <div className='absolute -top-4 -right-4'>
             <button onClick={onClose}><Image src='/images/close.png' width={30} height={30} alt='Close icon' /></button>
